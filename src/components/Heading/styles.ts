febc9db49b48a29705b7ae04435ac4eda3f18d63
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2021-06-10 18:11:10 -0300

    Adjusts heading

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,21 +1,37 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 
-import { HeadingProps } from '.'
+import { HeadingProps, LineColors } from '.'
 
 const wrapperModifiers = {
-    lineLeft: (theme: DefaultTheme) => css`
-        border-left: 0.7rem solid ${theme.colors.secondary};
+    small: (theme: DefaultTheme) => css`
+        font-size: ${theme.font.sizes.medium};
+
+        &::after {
+            width: 3rem;
+        }
+    `,
+
+    medium: (theme: DefaultTheme) => css`
+        font-size: ${theme.font.sizes.xlarge};
+
+        ${media.greaterThan('medium')`
+            font-size: ${theme.font.sizes.xxlarge};
+        `}
+    `,
+
+    lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
+        border-left: 0.7rem solid ${theme.colors[lineColor]};
         padding-left: ${theme.spacings.xxsmall};
     `,
 
-    lineBottom: (theme: DefaultTheme) => css`
+    lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
         position: relative;
         margin-bottom: ${theme.spacings.medium};
 
         &::after {
             content: '';
-            border-bottom: 0.5rem solid ${theme.colors.primary};
+            border-bottom: 0.5rem solid ${theme.colors[lineColor]};
             bottom: -1rem;
             left: 0;
             position: absolute;
@@ -25,7 +41,7 @@ const wrapperModifiers = {
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
-    ${({ theme, color, lineLeft, lineBottom }) => css`
+    ${({ theme, color, lineLeft, lineBottom, lineColor, size }) => css`
         color: ${theme.colors[color!]};
         font-size: ${theme.font.sizes.xxlarge};
 
@@ -33,7 +49,8 @@ export const Wrapper = styled.h2<HeadingProps>`
             font-size: ${theme.font.sizes.xxlarge};
         `}
 
-        ${lineLeft && wrapperModifiers.lineLeft(theme)}
-        ${lineBottom && wrapperModifiers.lineBottom(theme)}
+        ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)}
+        ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor!)}
+        ${!!size && wrapperModifiers[size](theme)}
     `}
 `
